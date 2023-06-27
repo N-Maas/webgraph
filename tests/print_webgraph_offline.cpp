0db1e0658ee63c86202ceb1afdaf27aefe2b5cfe
Nikolai Maas <nikolai.maas@kit.edu>
2023-06-27 17:09:00 +0200

    adjust print_webgraph.cpp to output metis format with additional options
    - outputs undirected graph by default, use -d for directed
    - the -r options allows to remove degree zero nodes

--- tests/print_webgraph_offline.cpp
+++ tests/print_webgraph_offline.cpp
@@ -17,6 +17,8 @@
  *
  */
 
+#include <boost/program_options.hpp>
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -29,53 +31,105 @@
 int main( int argc, char** argv ) {
    using namespace std;
    using namespace webgraph::bv_graph;
+   namespace po = boost::program_options;
 
-   if( argc < 2 ) {
-      cerr << "You have to provide name of graph file.\n";
-      return 1;
+   using graph_ptr = graph::graph_ptr;
+
+   std::string name;
+   bool directed = false;
+   bool remove_degree_zero = false;
+
+   po::options_description options("Options");
+   options.add_options()
+      ("graph,g", po::value<std::string>(&name)->value_name("<string>")->required(), "Graph basename")
+      ("directed,d", po::bool_switch(&directed), "only output directed edges (without inserting the backwards edge)")
+      ("remove-degree-zero,r", po::bool_switch(&remove_degree_zero), "remove degree zero nodes");
+
+   po::variables_map cmd_vm;
+   po::store(po::parse_command_line(argc, argv, options), cmd_vm);
+   po::notify(cmd_vm);
+
+   if (directed && remove_degree_zero) {
+      std::cerr << "Error: -d and -r are not compatible." << std::endl;
+      std::exit(1);
    }
-   
-   string name = argv[1];
-
-   typedef boost::shared_ptr<graph> graph_ptr;
 
    graph_ptr gp = graph::load_offline( name );
    
-//   cerr << "about to try to get node iterator.\n";
-   
-   graph::node_iterator n, n_end;
-   
-   tie(n, n_end) = gp->get_node_iterator( 0 );
-   
-//   cerr << "here are vertices:\n";
-   
-   cerr << gp->get_num_nodes() << endl;
-   
+   size_t num_nodes = gp->get_num_nodes();
+   size_t num_arcs = gp->get_num_arcs();
+
+   std::vector<std::vector<int>> adjacency_lists(num_nodes);
+   std::vector<size_t> index_mapping(num_nodes);
+   auto check_is_in_range = [&](const size_t index) {
+      if (index > adjacency_lists.size()) {
+         std::cerr << "Error: size mismatch." << std::endl;
+         std::exit(1);
+      }
+   };
+
+   auto [n, n_end] = gp->get_node_iterator( 0 );
+   size_t i = 0;
+   size_t undirected_edges = 0;
    while( n != n_end ) {
-//       webgraph::bv_graph::graph::successor_iterator succ, succ_end;
+      check_is_in_range(i);
 
-//       tie( succ, succ_end ) = successors( n );
-      vector<int> succ = successor_vector( n );
-      
-//      cerr << "outdegree : " << outdegree(n) << endl;
-
-//      cerr << "********************   " << *n << "\n";
-
-//       copy( succ.begin(), succ.end(), ostream_iterator<int>(cerr, " " ) );
-      if( succ.size() > 0 ) 
-         cerr << succ[0];
-
-      for( unsigned i = 1; i < succ.size(); ++i )
-         cerr << " " << succ[i];
-      
-//       ostringstream strstr;
-//       copy( succ.begin(), succ.end(), ostream_iterator<int>(strstr, " " ) );
-
-//      logs::logger( "print_webgraph" ) << strstr.str() << "\n";
-
-      cerr << endl;
-
+      std::vector<int> outgoing = successor_vector( n );
+      std::sort(outgoing.begin(), outgoing.end());
+      if (directed) {
+         adjacency_lists[i] = std::move(outgoing);
+      } else {
+         // important note: adjacency_lists[i] is currently sorted
+         // due to the way we are constructing the lists
+         size_t j = 0;
+         for (int t: outgoing) {
+            size_t target = static_cast<size_t>(t);
+            if (target > i) {
+               check_is_in_range(target);
+               adjacency_lists[i].push_back(target);
+               adjacency_lists[target].push_back(i);
+               undirected_edges++;
+            } else if (target < i) {
+               // check whether the edge was already inserted
+               while (j < adjacency_lists[i].size() && adjacency_lists[i][j] < t) {
+                  ++j;
+               }
+               if (j == adjacency_lists[i].size() || adjacency_lists[i][j] != t) {
+                  // edge not inserted yet
+                  adjacency_lists[i].push_back(target);
+                  adjacency_lists[target].push_back(i);
+                  undirected_edges++;
+               }
+            }
+         }
+      }
+      ++i;
       ++n;  
+   }
+
+   if (remove_degree_zero) {
+      size_t new_index = 0;
+      for (size_t i = 0; i < adjacency_lists.size(); ++i) {
+         index_mapping[i] = new_index;
+         if (adjacency_lists[i].size() > 0) {
+            new_index++;
+         }
+      }
+      num_nodes = new_index;
+   }
+
+   std::cout << num_nodes << " " << (directed ? num_arcs : undirected_edges) << std::endl;
+   for (const auto& outgoing: adjacency_lists) {
+      for (int t: outgoing) {
+         size_t target = static_cast<size_t>(t);
+         if (remove_degree_zero) {
+            target = index_mapping[target];
+         }
+         std::cout << target << " ";
+      }
+      if (!remove_degree_zero || outgoing.size() > 0) {
+         std::cout << std::endl;
+      }
    }
 
    return 0;  
